e6cae5d98cc92799836474c86a1da128d7cc9ff9
etudiant <etudiant@d7P2>
2020-10-14 17:18:49 +0200

    Remis App.vue par default

--- lide-web/src/App.vue
+++ lide-web/src/App.vue
@@ -1,19 +1,15 @@
 <template>
 	<v-app>
 		<v-main>
-      <Explorer></Explorer>  
     </v-main>
 	</v-app>
 </template>
 
 <script>
-	import Explorer from "./components/Explorer.vue";
-
 	export default {
 		name: "App",
 
 		components: {
-      Explorer,
     },
 
 		data: () => ({}),

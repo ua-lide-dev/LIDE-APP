39f9eb650a839e1d7c82f9c257b311d5d3d4679b
etudiant <etudiant@d7P2>
2020-10-14 15:30:50 +0200

    Ajout des controllers

---
+++ lide-back/controllers/user.controller.js
@@ -0,0 +1,75 @@
+const User = require("../model/user");
+
+/*
+    Le controlleur User gère les appels à la bdd 
+        pour tout ce qui concerne les users (username + projets)
+
+*/
+
+// GET -> récupère un utilisateur
+exports.get = (req, res) => {
+    User.findOne({ // Fonction predefinie Mangoose
+      _id: req.params.idUser,
+    })
+      .then((user) => { // Si la requête réussi (Statut 200 -> OK)
+        res.status(200).json(project);
+    })
+      .catch((err) => { // Si la requête échoue (Statut 400 BAD REQUEST)
+        res.status(400).json(err);
+    });
+};
+
+// POST -> crée un utilisateur
+exports.create = (req, res) => {
+    delete req.body._id;  // Sécurité, l'id sera généré par mangoDB
+  
+    // On initialise un nouvel objet User
+    const user = new User({
+      ...req.body,    // Syntaxe => cast des infos req.body.* 
+    });
+  
+    user
+      .save() // Fonction mangoose pour l'ajout en base
+      .then(() => {
+        res.status(201).json({ // Si la requête réussi (Statut 201 -> CREATED)
+          message: "L'utilisateur a bien été ajouté !",
+        });
+      })
+      .catch((err) => { // Si la requête échoue (Statut 400 BAD REQUEST)
+        res.status(400).json({
+          error: err,
+        });
+    });
+};
+
+// PUT -> Modifie un utilisateur
+exports.update = (req, res) => {
+    User.updateOne({ _id: req.params.idUser }, { ...req.body})
+      .then(() => {
+        res.status(201).json({ // Si la requête réussi (Statut 201 -> CREATED)
+          message: "L'utilisateur a été mis à jour",
+        });
+    })
+    .catch((err) => { // Si la requête échoue (Statut 400 BAD REQUEST)
+        res.status(400).json({
+          error: err,
+        });
+    });
+};
+  
+// DELETE -> Supprime un utilisateur
+exports.delete = (req, res) => {
+    User.findOneAndDelete({
+      _id: req.params.idUser,
+    })
+      .then(() => { // Si la requête réussi (Statut 200 -> OK)
+        res.status(200).json({
+          message: "L'utilisateur a été supprimé !",
+        });
+      })
+      .catch((err) => { // Si la requête échoue (Statut 400 BAD REQUEST)
+        res.status(400).json({
+          error: err,
+        });
+    });
+};
--- lide-back/router.js
+++ lide-back/router.js
@@ -2,10 +2,31 @@ const express = require("express");
 const router = express.Router();
 
 // Controllers
+const user = require("./controller/user.controller");
+const project = require("./controller/project.controller");
+const file = require("./controller/file.controller");
 
 /* --- Routes --- */
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Routes User
+router.get("/user/:idUser", (req, res) => {user.get});
+router.post("/user", (req, res) => {user.create});
+router.put("/user/:idUser", (req, res) => {user.update});
+router.delete("/user/:idUser", (req, res) => {user.delete});
+
+//Routes Project
+router.get("/user/:idUser/project/:idProject", (req, res) => {project.get});
+router.get("/user/:idUser/project", (req, res) => {project.getAll});
+router.post("/user/:idUser/project", (req, res) => {project.create});
+router.put("/user/:idUser/project/:idProject", (req, res) => {project.update});
+router.delete("/user/:idUser/project/:idProject", (req, res) => {project.delete});
+router.delete("/user/:idUser/project", (req, res) => {project.deleteAll});
+
+//Routes File
+router.get("/user/:idUser/project/:idProject/file/:idFile", (req, res) => {file.get});
+router.get("/user/:idUser/project/:idProject/file", (req, res) => {file.getAll});
+router.post("/user/:idUser/project/:idProject", (req, res) => {file.create});
+router.put("/user/:idUser/project/:idProject/file/:idFile", (req, res) => {file.update});
+router.delete("/user/:idUser/project/:idProject/file/:idFile", (req, res) => {file.delete});
+router.delete("/user/:idUser/project/:idProject/file", (req, res) => {file.deleteAll});
 
 module.exports = router;

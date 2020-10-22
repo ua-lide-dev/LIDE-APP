b085b5bb83c41b9fdd1a8b13766736a9b970c590
etudiant <etudiant@d2L2>
2020-10-22 12:44:41 +0200

    changement de branche pour pouvoir merge

--- lide-web/src/pages/Home.vue
+++ lide-web/src/pages/Home.vue
@@ -1,7 +1,38 @@
-<template></template>
+<template><v-app>
+  <v-app-bar app color="blue">
+    <Header/>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main class="py-9">
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid >
+      <Body/>
+    </v-container>
+  </v-main>
+
+  <v-footer app color="blue">
+    <Footer/>
+  </v-footer>
+</v-app>
+</template>
 
 <script>
-export default {};
+ import Header from "../components/header"
+ import Body from "../components/body"
+import Footer from "../components/footer"
+export default {
+  name: "App",
+
+  components: {
+    Header,
+     Body,
+     Footer
+    },
+
+  data: () => ({})
+};
 </script>
 
 <style>

27684198834d5094d9f075031fbbc47cf2708c64
etudiant <etudiant@d2L2>
2020-10-22 13:10:53 +0200

    fin du transfert de l'ancienne branche vers la nouvelle

--- lide-web/src/App.vue
+++ lide-web/src/App.vue
@@ -1,15 +1,18 @@
 <template>
-	<v-app>
+	<Home/>
+	<!-- <v-app>
 		<v-main>
-    </v-main>
-	</v-app>
+		</v-main>
+	</v-app> -->
 </template>
 
 <script>
+	import Home from "./pages/Home.vue"
 	export default {
 		name: "App",
 
 		components: {
+			Home
     },
 
 		data: () => ({}),

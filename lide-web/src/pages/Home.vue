58d0315b1561b496f2cfd511f604f7c6cbc2baad
Quentin <quentin@d2L2>
2020-10-28 18:33:02 +0100

    ajout de la page Home

---
+++ lide-web/src/pages/Home.vue
@@ -0,0 +1,37 @@
+<template><v-app>
+  <v-app-bar app color="blue">
+    <Header/>
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main class="py-9">
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid >
+      <Body/>
+    </v-container>
+  </v-main>
+
+  <v-footer app color="blue">
+    <Footer/>
+  </v-footer>
+</v-app>
+</template>
+
+<script>
+ import Header from "../components/Header"
+ import Body from "../components/Body"
+import Footer from "../components/Footer"
+export default {
+  name: "App",
+  components: {
+    Header,
+     Body,
+     Footer
+    },
+  data: () => ({})
+};
+</script>
+
+<style>
+</style>

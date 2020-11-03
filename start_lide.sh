45c5a9d0921dd01527d58ba66606895e06bc4d2c
ThibaudViret <thib.viret85@gmail.com>
2020-11-03 20:34:36 +0100

    maj script

--- start_lide.sh
+++ start_lide.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
-docker-compose stop
-docker-compose up -d --remove-orphans && 
-docker-compose logs lide-back-service >> ./lide-back/back_logs.log 
+if [ "$1" = "-f" ]
+then 
+    echo " >using force"
+    docker-compose down --remove-orphans -v 
+    docker image rm lide-web lide-back
+    docker-compose up -d --force-recreate -V && 
+    docker-compose logs lide-back-service >> ./lide-back/back_logs.log 
+
+else
+    docker-compose up -d && 
+    docker-compose logs lide-back-service >> ./lide-back/back_logs.log 
+fi
+
+
+

d9014436e764dd5006ffea6fe6359bc3af94e1db
Quentin <quentin@d2L2>
2020-11-14 11:56:58 +0100

    squellette du controller

--- lide-back/controllers/compil.controller.js
+++ lide-back/controllers/compil.controller.js
@@ -1,1 +1,17 @@
-//first push
+const { spawn } = require('child_process');
+
+//demande de compil de la part de user
+
+//recup de user : $USER
+
+//recup du fichier user : $FILE 
+
+
+//lancement de la bonne image docker en fonction de l'extension 
+//commande de run a passer a childprocess pour chaque user, pour l'image cpp
+//run --rm -it --name cpp$USER -v /root/data/$USER/$FILE:/$FILE cpp $FILE
+//pour les autres images on a juste a changer cpp par le nom du langage
+
+//recuperation du resultat de childprocess 
+
+//renvoie du res pour que Xtern affiche qqlchose

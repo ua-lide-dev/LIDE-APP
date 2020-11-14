36581bf81156b897eaa144c91e59682c56616a0e
Quentin <quentin@d2L2>
2020-11-14 18:15:59 +0100

    changement mineur

--- lide-back/controllers/compil.controller.js
+++ lide-back/controllers/compil.controller.js
@@ -1,9 +1,10 @@
 //demande de compil de la part de user
 
-//recup de user : $USER
+//recup de user : $username
 
-//recup du fichier user : $FILE 
+//recup du path pour le fichier : $projetpath
 
+//recup du fichier user : $file_name 
 
 //lancement de la bonne image docker en fonction de l'extension 
 //commande de run a passer a childprocess pour chaque user, pour l'image cpp
@@ -11,7 +12,7 @@
 //pour les autres images on a juste a changer cpp par le nom du langage
 
 const { exec } = require('child_process');
-exec('run --rm -it --name cpp$USER -v /root/data/$USER/$FILE:/$FILE cpp $FILE', (error, stdout, stderr) => {
+exec('run --rm -it --name cpp$USER -v /data-lide/$username/$projetpath/$file_name. :/$file_name cpp $file_name', (error, stdout, stderr) => {
   if (error) {
     //on revoi msg d'erreur la
     console.error(`exec error: ${error}`);
@@ -23,7 +24,5 @@ exec('run --rm -it --name cpp$USER -v /root/data/$USER/$FILE:/$FILE cpp $FILE', 
   //recupertation des erreus on fait pas la diff avec la reussite 
   console.error(`stderr: ${stderr}`);
 });
-
-
 
 //renvoie du res pour que Xtern affiche qqlchose

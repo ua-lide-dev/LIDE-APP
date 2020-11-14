c2ced80620a39e9e5af4866597e16525f9074ca5
Quentin <quentin@d2L2>
2020-11-14 11:07:24 +0100

    entrypoint et image gcc

--- lide-back/images/cpp/buildNrun.sh
+++ lide-back/images/cpp/buildNrun.sh
@@ -2,13 +2,13 @@
 #c'est pas bin/bash avec alpine ! c'est bin/sh
 
 ########################################################################
-#								       #
+#								      								   #
 #         SCRIPT BASH QUI COMPILE ET EXECUTE UN FIHICER CPP            #
-#								       #
+#								        							   #
 ########################################################################
 
 
 #si la compile est sans erreurs alors on exec a.out et on le rm
-if g++ -std=c++11 -Wpedantic /data/$1 -o a.out
+if g++ -std=c++11 -Wpedantic $1 -o a.out
 then ./a.out; rm a.out
 fi

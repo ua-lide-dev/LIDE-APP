8421907be5b0cb47f8b180820e87f2e5c5a30ccd
Quentin <quentin@d2L2>
2020-11-14 19:24:08 +0100

    ajout du fichier sur le serv

--- lide-back/controllers/compil.controller.js
+++ lide-back/controllers/compil.controller.js
@@ -19,9 +19,16 @@ exports.get = (req, res) => {
 //commande de run a passer a childprocess pour chaque user, pour l'image cpp
 //run --rm -it --name cpp$USER -v /root/data/$USER/$FILE:/$FILE cpp $FILE
 //pour les autres images on a juste a changer cpp par le nom du langage
+
+//mkdir du nouveau dir
 exec('mkdir le nouveau dir avec les fichiers'), (error, stdout, stderr) => {}
 
 
+//creation du fichier
+exec('echo body > path jusqu au fichier'), (error, stdout, stderr) => {}
+
+
+//exec de docker 
 exec('docker run --rm -it --name cpp' + req.Header.username + '$username -v /data-lide/' + req.Header.username + '/' + req.body.projectpath 
     + '/' + req.body.file_name + ':/' + req.body.file_name + ' ' + req.body.file_name, 
     (error, stdout, stderr) => {

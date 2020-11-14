6488b005ce7910c4be0487d95d92cdb003caff7e
Quentin <quentin@d2L2>
2020-11-14 12:02:53 +0100

    ajout commentaire

--- lide-back/controllers/compil.controller.js
+++ lide-back/controllers/compil.controller.js
@@ -13,13 +13,17 @@
 const { exec } = require('child_process');
 exec('run --rm -it --name cpp$USER -v /root/data/$USER/$FILE:/$FILE cpp $FILE', (error, stdout, stderr) => {
   if (error) {
+    //on revoi msg d'erreur la
     console.error(`exec error: ${error}`);
     return;
   }
+
+  //recuperation du resultat de childprocess et on l'envoi 
   console.log(`stdout: ${stdout}`);
+  //recupertation des erreus on fait pas la diff avec la reussite 
   console.error(`stderr: ${stderr}`);
 });
 
-//recuperation du resultat de childprocess 
+
 
 //renvoie du res pour que Xtern affiche qqlchose

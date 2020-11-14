2b09d8f2f825742c00b2262f3db156f59d4ade32
Quentin <quentin@d2L2>
2020-11-14 18:45:47 +0100

    ajout des route

--- lide-back/controllers/compil.controller.js
+++ lide-back/controllers/compil.controller.js
@@ -1,8 +1,10 @@
 //demande de compil de la part de user
 
+// GET -> récupère un fichier
+exports.get = (req, res) => {
 //recup de user : $username
 //recup du path pour le fichier : $projetpath
-//recup du fichier user : $file_name 
+//recup du fichier user : $file_name
 
 //lancement de la bonne image docker en fonction de l'extension 
 //commande de run a passer a childprocess pour chaque user, pour l'image cpp
@@ -11,15 +13,28 @@
 
 const { exec } = require('child_process');
 exec('docker run --rm -it --name cpp$USER -v /data-lide/$username/$projetpath/$file_name:/$file_name cpp $file_name', (error, stdout, stderr) => {
-  if (error) {
+  
+  if(error){
     //on revoi msg d'erreur la
     console.error(`exec error: ${error}`);
+    res.status(400).json(${error});
     return;
   }
+
   //recuperation du resultat de childprocess et on l'envoi 
-  console.log(`stdout: ${stdout}`);
+  if(stdout){
+    console.log(`stdout: ${stdout}`);
+    return;
+  }
+  
   //recupertation des erreus on fait pas la diff avec la reussite 
-  console.error(`stderr: ${stderr}`);
+  if(stderr){
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+
 });
 
 //renvoie du res pour que Xtern affiche qqlchose
+};
+

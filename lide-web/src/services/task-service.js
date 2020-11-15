f7d34564533ee8014879c3546ecdba2b54a019f1
etudiant <etudiant@d7P2>
2020-11-15 03:21:49 +0100

    LIDE-52 Modification du format des requêtes http envoyées au back

--- lide-web/src/services/task-service.js
+++ lide-web/src/services/task-service.js
@@ -7,44 +7,60 @@ import http from "./http-config";
 
 /// Projects
 
-function getProjects(idUser) {
-  return http.get("/user/" + idUser + "/project");
+function getProjects(username) {
+  http.setHeader("username", username)
+
+  return http.get("/getProjects");
 }
 
-function createProject(idUser, data) {
-  return http.post("/user/" + idUser + "/project", data);
+function createProject(username, data) {
+  http.setHeader("username", username)
+
+  return http.post("/createProject", data);
 }
 
-function renameProject(idUser, idProject, data) {
-  return http.put("/user/" + idUser + "/project/" + idProject, data);
+function renameProject(username, projectpath, data) {
+  http.setHeader("username", username)
+  http.write("{\"projectpath\":"+ projectpath +"}");
+
+  return http.put("/renameProject", data);
 }
 
-function deleteProject(idUser, idProject) {
-  if(idProject != "" && idProject != null)
-  {
-    return http.delete("/user/" + idUser + "/project/" + idProject);
-  }
+function deleteProject(username, projectpath) {
+  http.setHeader("username", username)
+  http.write("{\"projectpath\":"+ projectpath +"}");
+
+  return http.delete("/deleteProject");
 }
 
 /// Files
 
-function createFile(idUser, idProject, data) {
-  return http.post("/user/" + idUser + "/project/" + idProject, data);
+function createFile(username, projectpath, data) {
+  http.setHeader("username", username)
+  http.write("{\"projectpath\":"+ projectpath +"}");
+
+  return http.post("/createFile", data);
 }
 
-function updateFile(idUser, idProject, idFile, data) {
-  return http.put("/user/" + idUser + "/project/" + idProject + "/file/" + idFile, data);
+function updateFile(username, projectpath, filename, data) {
+  http.setHeader("username", username)
+  http.write("{\"projectpath\":" + projectpath + ",\"filename\":" + filename + "}");
+
+  return http.put("/updateFile", data);
 }
 
-function deleteFile(idUser, idProject, idFile) {
-  if(idFile != "" && idFile != null)
-  {
-    return http.delete("/user/" + idUser + "/project/" + idProject + "/file/" + idFile);
-  }
+function deleteFile(username, projectpath, filename) {
+  http.setHeader("username", username)
+  http.write("{\"projectpath\":" + projectpath + ",\"filename\":" + filename + "}");
+
+  return http.delete("/deleteFile");
 }
 
-function saveFile(idFile, data) {
-  return http.delete("/save/file/" + idFile, data);
+function saveFile(username, projectpath, filename, data) {
+  http.setHeader("username", username)
+  http.write("{\"projectpath\":" + projectpath + ",\"filename\":" + filename + "}");
+
+  return http.delete("/saveFile", data);
 }
 
 

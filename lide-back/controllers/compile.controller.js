dc0773707a86b41728a7f8dfe0b4b5502a3b8c05
etudiant <etudiant@d7P2>
2020-11-18 16:53:19 +0100

    LIDE-41 ajout de la gestion de suppression des containeurs préexistant et renvoie de l'id du containeur

--- lide-back/controllers/compile.controller.js
+++ lide-back/controllers/compile.controller.js
@@ -10,10 +10,10 @@ var file = req.body.filename;
 var ext = req.body.ext; 
 var path = req.body.projectpath; 
 
-console.log("user : " + user);
-console.log("file : " + file);
-console.log("ext : " + ext);
-console.log("path" + path);
+console.log(user);
+console.log(file);
+console.log(ext);
+console.log(path);
 
 //mkdir du nouveau dir
 execSync('mkdir -p /data-lide/'+ user +'/' + path);
@@ -22,8 +22,18 @@ execSync('mkdir -p /data-lide/'+ user +'/' + path);
 execSync('echo "#include<iostream>\n int main () {std::cout << 12 << std::endl; return 0;}" > /data-lide/'+ user +'/' + path + '/' +file + "." + ext);
 
 //exec de docker 
+
 //pour changer de langage on peut juste modifier le ext en req.body.ext 
-//docker run -i --name qmaignan --rm -v /data-lide/qmaignan/TP1/fichier.cpp:/fichier.cpp cpp fichier.cpp : exemple de docker run
-res.status(200).json(execSync("docker run -i --rm --name "+user+" -v /data-lide/"+user+"/"+path+"/"+file+"."+ext+" "+ ext+" "+file+"."+ext).stdout);
-execSync("docker inspect --format='{{.Id}}' "+ user);
+//docker run -i --name cppqmaignan --rm -v /data-lide/qmaignan/TP1/fichier.cpp:/fichier.cpp cpp fichier.cpp : exemple de docker run
+try {
+  execSync("docker container rm " + user);
+} catch (error) {
+  console.log(error);
+}
+
+
+execSync("docker run -i --name "+user+" -v /data-lide/"+user+"/"+path+"/"+file+"."+ext+" "+ ext+" "+file+"."+ext);
+
+var idDocker = execSync("docker inspect --format='{{.Id}}' "+ user).toString();
+res.status(200).json(idDocker);
 };

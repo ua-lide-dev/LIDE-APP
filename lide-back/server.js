09a5896687a646bcc219d028ac250c868f838d00
Quentin <quentin@d2L2>
2020-11-18 14:53:50 +0100

    maxence maj

--- lide-back/server.js
+++ lide-back/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 
-app.use(express.json())
+app.use(express.json());
 
 /* -- Connection à la base de donnée MongoDB --- */
 db.connect();

e60528b0adf6c20905bac63a708327f1e6327c58
Quentin <quentin@d2L2>
2020-11-18 16:36:15 +0100

    ajout de la recup de l'id pour le conteneur

--- lide-back/controllers/compile.controller.js
+++ lide-back/controllers/compile.controller.js
@@ -10,21 +10,20 @@ var file = req.body.filename;
 var ext = req.body.ext; 
 var path = req.body.projectpath; 
 
-console.log(user);
-console.log(file);
-console.log(ext);
-console.log(path);
+console.log("user : " + user);
+console.log("file : " + file);
+console.log("ext : " + ext);
+console.log("path" + path);
 
 //mkdir du nouveau dir
 execSync('mkdir -p /data-lide/'+ user +'/' + path);
 
 //creation du fichier
-execSync('echo "#include<iostream>\n int main () {std::cout << 12 << std::endl; return 0;}" > /data-lide/'+ user +'/' + path + '/' +file);
+execSync('echo "#include<iostream>\n int main () {std::cout << 12 << std::endl; return 0;}" > /data-lide/'+ user +'/' + path + '/' +file + "." + ext);
 
 //exec de docker 
-
 //pour changer de langage on peut juste modifier le ext en req.body.ext 
-//docker run -i --name cppqmaignan --rm -v /data-lide/qmaignan/TP1/fichier.cpp:/fichier.cpp cpp fichier.cpp : exemple de docker run
-execSync("docker run -i --rm --name "+user+" -v /data-lide/"+user+"/"+path+"/"+file+"."+ext+" "+ ext+" "+file+"."+ext);
-execSync("docker inspect --format='{{.Id}}'"+ user);
+//docker run -i --name qmaignan --rm -v /data-lide/qmaignan/TP1/fichier.cpp:/fichier.cpp cpp fichier.cpp : exemple de docker run
+res.status(200).json(execSync("docker run -i --rm --name "+user+" -v /data-lide/"+user+"/"+path+"/"+file+"."+ext+" "+ ext+" "+file+"."+ext).stdout);
+execSync("docker inspect --format='{{.Id}}' "+ user);
 };
--- lide-back/images/cpp/buildNrun.sh
+++ lide-back/images/cpp/buildNrun.sh
@@ -7,8 +7,8 @@
 #								        							   #
 ########################################################################
 
-echo "#include<iostream> \nint main () {std::cout << 12 << std::endl; return 0;}" > fichier.cpp
+
 #si la compile est sans erreurs alors on exec a.out et on le rm
-if g++ -std=c++11 -Wpedantic fichier.cpp -o a.out
+if g++ -std=c++11 -Wpedantic $1 -o a.out
 then ./a.out; rm a.out
 fi

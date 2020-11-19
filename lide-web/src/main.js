2240ef49e7073124aa0249466281ae001cc57b0f
Quentin <quentin@d2L2>
2020-11-19 15:50:22 +0100

    store dans le main, ajout des getters et setters pour les variables demandées

--- lide-web/src/main.js
+++ lide-web/src/main.js
@@ -1,23 +1,70 @@
 import Vue from "vue";
+import vuex from "vuex";
+import persistedstate from "vuex-persistedstate";
 import App from "./App.vue";
 import router from "./configs/router";
 import vuetify from "./configs/vuetify";
 import axios from "axios";
-import store from "./store/store.js";
+//import store from "./store/store.js";
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
+Vue.use(vuex);
+
+const store = new vuex.Store({
+
+  plugins: [persistedstate()],
+
+  state: {
+      username: String,
+      project: Array,
+      currentFile: Object,
+      tabs: Array,
+  },
+  
+  mutations: {
+    setUsername(state, payload){
+      state.username = payload;
+    },
+    setProject(state, payload){
+      state.project = payload;
+    },
+    setCurrentFile(state, payload){
+      state.currentFile = payload;
+    },
+    setTabs(state, payload){
+      state.tabs = payload;
+    }
+  },
+  
+  actions: {
+
+  },
+  
+  getters: {
+    getUsername(state){
+      return state.username;
+    },
+    getProject(state){
+      return state.project;
+    },
+    getCurrentFile(state){
+      return state.currentFile;
+    },
+    getTabs(state){
+      return state.tabs;
+    }
+  }
+
+});
 
 new Vue({
   vuetify,
   router,
-  store,
   render: (h) => h(App),
 
-  compted:{
-    message(){
-        return store.state.message;
-    }
-}
+
 
 }).$mount("#app");
+
+console.log(store);
--- lide-web/src/store/store.js
+++ lide-web/src/store/store.js
@@ -1,18 +1,1 @@
-import vuex from "vuex";
 
-const store = new vuex.store({
-    state: {
-        message: "hello world",
-    },
-    mutations: {
-
-    },
-    actions: {
-
-    },
-    getters: {
-
-    }
-});
-
-console.log(store);

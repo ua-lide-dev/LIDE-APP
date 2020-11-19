1530e1a8830a89191a48f4b5f7e47b6812ce2a05
Quentin <quentin@d2L2>
2020-11-19 14:41:21 +0100

    ajout de vuex au main.js

--- lide-web/src/main.js
+++ lide-web/src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./configs/router";
 import vuetify from "./configs/vuetify";
 import axios from "axios";
+import store from "./store/store.js";
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
@@ -10,5 +11,13 @@ Vue.prototype.$http = axios;
 new Vue({
   vuetify,
   router,
+  store,
   render: (h) => h(App),
+
+  compted:{
+    message(){
+        return store.state.message;
+    }
+}
+
 }).$mount("#app");
--- lide-web/src/store/store.js
+++ lide-web/src/store/store.js
@@ -1,1 +1,18 @@
-//first commit
+import vuex from "vuex";
+
+const store = new vuex.store({
+    state: {
+        message: "hello world",
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
+    getters: {
+
+    }
+});
+
+console.log(store);

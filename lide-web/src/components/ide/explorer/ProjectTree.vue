ab4d8f9d8ddfc3e0a948b8f98b80a66ab53f6c27
Quentin <quentin@d2L2>
2020-12-02 17:16:48 +0100

    fix erreur tree

--- lide-web/src/components/ide/explorer/ProjectTree.vue
+++ lide-web/src/components/ide/explorer/ProjectTree.vue
@@ -92,8 +92,6 @@ export default {
       }
       //mise a jour du current file et ajout dans les tabs
       this.$store.dispatch('getFile',obj);
-      this.$store.commit("SET_CURRENTPROJECTNAME", this.projectName);
-       
     },
     filesMenu: function (e, item) {
       //ouvrir le ContextMenu

d1c9988ef3f472360c266e8c5e94cd6182abd18e
GuillaumePoiron <guillaume.poiron@outlook.fr>
2021-01-29 20:51:30 +0100

    ajout du composant notification

---
+++ lide-web/src/components/utils/Notification.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-snackbar
+			v-model="show"
+			:multi-line="multiLine"
+			top
+			:color="color"
+		>
+			{{ msg }}
+
+			<template v-slot:action="{ attrs }">
+				<v-btn
+				icon
+				v-bind="attrs"
+				@click="show = false"
+				>
+				<v-icon >mdi-close-circle-outline</v-icon>
+				</v-btn>
+			</template>
+		</v-snackbar>
+</template>
+
+<script>
+export default {
+	props: ['msg', 'color'],
+	data() {
+		return {
+			show: false
+		};
+	},
+	created() {
+		this.$root.$refs.Notification = this;
+	}
+}
+</script>
+
+<style>
+</style>

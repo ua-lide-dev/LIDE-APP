aa3e6764f291dd64e43a41f200afe8f7a906a7ed
root <root@projet-lide.ETUD>
2021-01-30 02:33:05 +0100

    reformat

--- lide-web/src/components/utils/Notification.vue
+++ lide-web/src/components/utils/Notification.vue
@@ -1,36 +1,27 @@
 <template>
-  <v-snackbar
-			v-model="show"
-			:multi-line="multiLine"
-			top
-			:color="color"
-		>
-			{{ msg }}
+	<v-snackbar v-model="show" :multi-line="multiLine" top :color="color">
+		{{ msg }}
 
-			<template v-slot:action="{ attrs }">
-				<v-btn
-				icon
-				v-bind="attrs"
-				@click="show = false"
-				>
-				<v-icon >mdi-close-circle-outline</v-icon>
-				</v-btn>
-			</template>
-		</v-snackbar>
+		<template v-slot:action="{ attrs }">
+			<v-btn icon v-bind="attrs" @click="show = false">
+				<v-icon>mdi-close-circle-outline</v-icon>
+			</v-btn>
+		</template>
+	</v-snackbar>
 </template>
 
 <script>
 export default {
-	props: ['msg', 'color'],
+	props: ["msg", "color"],
 	data() {
 		return {
-			show: false
+			show: false,
 		};
 	},
 	created() {
 		this.$root.$refs.Notification = this;
-	}
-}
+	},
+};
 </script>
 
 <style>

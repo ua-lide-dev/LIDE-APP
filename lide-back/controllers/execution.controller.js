6fcf5c72ebd711ce732058a4adbb3471bb30ba86
Aloïs LARDEUX <alois.lardeux@gmail.com>
2021-01-31 20:10:41 +0100

    wip multi-file compil

--- lide-back/controllers/execution.controller.js
+++ lide-back/controllers/execution.controller.js
@@ -1,6 +1,7 @@
 const FileService = require('../services/db/file.service');
 const ProjectService = require('../services/db/project.service');
 const { execSync, spawn } = require('child_process');
+const fs = require('fs').promises;
 
 // Fonction de compilation & exécution
 exports.execute = async (req, res) => {
@@ -8,13 +9,18 @@ exports.execute = async (req, res) => {
   const fileid = req.params.fileid;
 
   // récupération du fichier
-  const file = await FileService.get(username, fileid).catch((error) => { res.status(200).json(error); });
+  const file = await FileService.get(username, fileid).catch((error) => {
+    console.error(error);
+    res.status(500).json(error);
+  });
   const filename = file.filename;
   const reqExtension = file.extension;
-  const fileContent = file.content;
 
   // récupération de son projet
-  const project = await ProjectService.get(username, file.projectid).catch((error) => { res.status(200).json(error); });
+  const project = await ProjectService.get(username, file.projectid).catch((error) => {
+    console.error(error);
+    res.status(500).json(error);
+  });
   const projectname = project.projectname;
 
   // params containter
@@ -22,59 +28,70 @@ exports.execute = async (req, res) => {
   let containerId = '';
 
   try {
-    const extension = reqExtension.replace('.', ''); //TODO A ENLEVER et adapter 
+    const extension = reqExtension.replace('.', ''); //FIXME A ENLEVER et adapter 
+    execSync('rm -rf /lide-data/' + username + '/');
     // Création du volume et du fichier
-    execSync('mkdir -p /lide-data/' + username + '/' + projectname);
+    const projectPath = '/lide-data/' + username + '/' + projectname + '/';
+    execSync('mkdir -p ' + projectPath);
 
-    filePath = '/lide-data/' + username + '/' + projectname + '/' + filename + '.' + extension;
+    for (const fileid of project.files) {
+      await FileService.get(username, fileid).catch((error) => {
+        console.error(error);
+        res.status(500).json(error);
+      }).then(async (file) => {
+        const extension = file.extension.replace('.', ''); //FIXME A ENLEVER et adapter 
+        // Remplir le contenu du fichier
+        const filePath = '/lide-data/' + username + '/' + projectname + '/' + file.filename + '.' + extension;
+        await fs.writeFile(filePath, file.content).catch((error) => {
+          console.error(error);
+        }).then(() => {
+          console.debug(`Write ${filePath}`);
+        })
 
-    // Remplir le contenu du fichier
-    execSync('echo \'' + fileContent + '\' > ' + filePath);
+      });
+    }
 
     // Supression d'un potentiel précedent container
     try {
       execSync('docker rm --force ' + containerName);
-    } catch (error) { }
-
-
+    } catch (error) {
+      console.error(error);
+    }
 
     // Sélection de l'image docker //TODO réupérer ça dynamiquement en base
     var img;
-
+    var startCommand;
     switch (extension) {
       case 'cpp':
         img = "cpp_lide";
+        startCommand = `docker run -it -d --cpus=1 -v ${projectPath}:/workdir --name ${containerName} ${img} ${projectPath}`;
+        break;
+      case 'h':
+        img = "cpp_lide";
+        startCommand = `docker run -it -d --cpus=1 -v ${projectPath}:/workdir --name ${containerName} ${img} ${projectPath}`;
         break;
       case 'java':
         img = "java_lide";
+        startCommand = `docker run -it -d --cpus=1 -v ${projectPath}:${projectPath} --name ${containerName} ${img} ${projectPath}${filename}.${extension}`;
         break;
       case 'py':
         img = "py_lide";
+        startCommand = `docker run -it -d --cpus=1 -v ${projectPath}:${projectPath} --name ${containerName} ${img} ${projectPath}${filename}.${extension}`;
         break;
-      case 'php':
-        img = "php_lide";
-        break;
-
       default:
-        res.status(400).response("Extension non gérée");
-
+        res.status(400).json("Extension non gérée");
     }
 
     // Lancement du conteneur
-    execSync(
-      'docker run -it -d --cpus=1' +
-      ' -v ' + filePath + ':/' + filename + '.' + extension +
-      ' --name ' + containerName +
-      ' ' + img +
-      ' ' + filename + '.' + extension
-    );
+    console.debug(startCommand);
+    execSync(startCommand);
 
     // Récupération de l'id du conteneur
     containerId = execSync('docker inspect --format=\'{{.Id}}\' ' + containerName).toString();
     containerId = containerId.substr(0, containerId.length - 1);
   } catch (error) {
     console.error(error);
-    res.status(400);
+    res.status(500).json(error);
   }
 
   res.status(200).json({ containerid: containerId });
@@ -91,6 +108,7 @@ exports.killExec = (req, res) => {
     console.debug(`container "${containerName}" killed`)
     res.status(200).json({ status: statusMessage });
   } catch (error) {
+    console.error(error);
     res.status(500).json(error.message);
   }
 }
--- lide-back/images/cpp/buildNrun.sh
+++ lide-back/images/cpp/buildNrun.sh
@@ -4,4 +4,9 @@ read socket
 
 TIMEOUT_VALUE=10
 
-g++ -std=c++11 -Wpedantic $1 -o program.out && timeout -s SIGKILL $TIMEOUT_VALUE ./program.out
+cd /workdir/
+
+g++ -std=c++11 -Wpedantic -c *.cpp &&
+g++ -o program.out *.o &&
+chmod +x program.out &&
+timeout -s SIGKILL $TIMEOUT_VALUE ./program.out

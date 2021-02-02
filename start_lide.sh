ab47f12207334d8eebd3a186b95b41b07c8424c7
root <root@projet-lide.ETUD>
2021-02-02 15:21:17 +0100

    add force with db save

--- start_lide.sh
+++ start_lide.sh
@@ -29,6 +29,13 @@ if [ "$1" = "-f" ]; then
     docker-compose up -d --force-recreate -V
     echo " >building images"
     build_images
+elif [ "$1" = "-fl" ]; then
+    echo " >using force (save db)"
+    docker-compose down --remove-orphans
+    docker image rm lide-web lide-back lide-wss
+    docker-compose up -d --force-recreate
+    echo " >building images"
+    build_images
 elif [ "$1" = "-i" ]; then
     echo " >building images"
     build_images

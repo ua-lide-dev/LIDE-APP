a1b764b469a61c5cd3c7b5567b49297add18adeb
ThibaudViret <34092775+ThibaudViret@users.noreply.github.com>
2021-02-04 03:14:32 +0100

    Update buildNrun.sh

--- lide-back/images/java/buildNrun.sh
+++ lide-back/images/java/buildNrun.sh
@@ -2,14 +2,12 @@
 
 read socket
 
-TIMEOUT_VALUE=300
-
 cd /workdir/ &&
 classname="$(echo $1 | cut -d'.' -f 1)" &&
 javac $1
 
 
-echo "sleep 10 && pkill -u root " > ./timeout && chmod +x ./timeout &&
+echo "sleep $TIMEOUT_VALUE && pkill -u root " > ./timeout && chmod +x ./timeout &&
 ./timeout & 
 
 java $classname

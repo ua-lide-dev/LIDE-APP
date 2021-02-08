a16accb5db6c530e5bf7c6a57894d7b80cedaddb
GuillaumePoiron <guillaume.poiron@outlook.fr>
2021-02-08 18:25:02 +0100

    notification s'affiche en haut de la page

--- lide-web/src/components/utils/Notification.vue
+++ lide-web/src/components/utils/Notification.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="text-center">
-    <v-snackbar v-model="snackbar" :timeout="timeout" :color="couleur">
+    <v-snackbar v-model="snackbar" top :timeout="timeout" :color="couleur">
       {{ text }}
       <template v-slot:action="{ attrs }">
-        <v-btn color="red" text v-bind="attrs" @click="close">Close</v-btn>
+        <v-btn icon v-bind="attrs" @click="close"
+          ><v-icon>mdi-close</v-icon></v-btn
+        >
       </template>
     </v-snackbar>
   </div>
@@ -13,17 +15,17 @@
 import { mapGetters, mapState } from "vuex";
 export default {
   methods: {
-    close: function() {
-        this.$store.dispatch("notification/closeNotif");
-    }
+    close: function () {
+      this.$store.dispatch("notification/closeNotif");
+    },
   },
   computed: {
     ...mapState({
-      snackbar: state => state.notification.actif,
-      text: state => state.notification.texte,
-      timeout: state => state.notification.timeout,
+      snackbar: (state) => state.notification.actif,
+      text: (state) => state.notification.texte,
+      timeout: (state) => state.notification.timeout,
       couleur: (state) => state.notification.couleur,
-    })
+    }),
   },
 };
 </script>
